3265aee15349f1d715cec044c71ebad9257c825c
ljv29 <jumpswervelogan@gmail.com>
2020-04-11 10:36:25 -0700

    refactored app to be more DRY, mobile responsive, and work 100% properly with a nice looking UI

--- index.js
+++ index.js
@@ -1,95 +1,131 @@
-$("#citysearchnow").on("click", function(event){
+$("#searchbtn").on("click", function (event) {
     event.preventDefault();
-    $(".citydisplayed").empty();
-    const cityName = $("#inputcity").val();
+    currentweatherajax();
+    $(".citydisplay").empty();
+});
+
+console.log(localStorage);
+
+function currentweatherajax() {
+    // calling the current forecast api on api.openweathermap.org
+    const cityName = $("#searchinput").val();
     const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&&APPID=f4c95ff5da6ac45aff62770ee6dbda0c'
     $.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-    const newBody = $("<div>");
-    const nameDateIcon = $("<div>")
-    const time = new Date((response.dt) * 1000 ).toLocaleDateString("en-US");
-    const date = $("<div>").text(response.name +' - '+ time);
-    nameDateIcon.append(date);
-    newBody.append(nameDateIcon);
-    const temp = $("<div>").text('Temperature: ' + response.main.temp + '°F');
-    const humid = $("<div>").text('Humidity: ' + response.main.humidity + '%');
-    const speed = $("<div>").text('Wind Speed: ' + response.wind.speed + ' MPH');
-    newBody.append(temp,humid,speed)
-    $(".citydisplayed").append(newBody);
-})
-});
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // making the citydisplay div which will contain the current weather, as well as the future 5 day forecast
+        
+        const jumbotron = $("<div class='card' id='citydisplayjumbotron'></div>");
+        const currentdisplay = $("<div class='currentdisplay'></div>");
+        const card1 = $("<div class='card my-3 mx-2' id='futurecard'></div>");
+        const cardbody1 = $("<div class='card-body'></div>");
+        const nameDateIcon = $("<h4 style='color: white;'>" + response.name + ", " + response.sys.country + " - " + "(" + new Date((response.dt) * 1000).toLocaleDateString("en-US") + ")" + "<img class='forecasticon' src=" + "'" + 'http://openweathermap.org/img/wn/' + response.weather[0].icon + ".png" + "'" + ">" + "</h4>");
+        const temp1 = $("<h5 style='color: white;'>" + "Temperature: " + response.main.temp + "°F" + "</h5>");
+        const humidity1 = $("<h5 style='color: white;'>" + "Humidity: " + response.main.humidity + "%" + "</h5>");
+        const windspeed1 = $("<h5 style='color: white;'>" + "Windspeed: " + response.wind.speed + " MPH" + "</h5>");
+        const today = $("<h2 class='my-3' style='color: white;'>Today's Forecast:</h2>");
+        cardbody1.append(nameDateIcon, temp1, humidity1, windspeed1);
+        card1.append(cardbody1);
+        currentdisplay.append(today, card1)
+        jumbotron.append(currentdisplay);
+        $(".citydisplay").append(jumbotron);
+        // making the historydisplay div which uses localStorage to display all recent searches and you may click them anytime to return to a previous search
+        // const button = $("<button type='button' id='historydisplaybtn' class='btn my-1 mx-1' style='background-color: lightblue; color: white;'>" + response.name + "</button>");
+        // const cities = [];
+        // cities.push(button);
+        // const JSONcity = JSON.stringify(cities);
+        // localStorage.setItem('cities',JSONcity);
+        // console.log(JSON.parse(localStorage['cities']));
 
-$("#citysearchnow").on("click", function(event){
-    event.preventDefault();
-    $(".day1").empty();
-    $(".day2").empty();
-    $(".day3").empty();
-    $(".day4").empty();
-    $(".day5").empty();
-    const cityName = $("#inputcity").val();
-    const queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&&APPID=f4c95ff5da6ac45aff62770ee6dbda0c'
-    $.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-    const newBody = $("<div>")
-    const time = new Date((response.list[0].dt) * 1000 ).toLocaleDateString("en-US");
-    const date = $("<div>").text(time);
-    const temp = $("<div>").text('Temp: ' + response.list[0].main.temp + '°F');
-    const humid = $("<div>").text('Humidity: ' + response.list[0].main.humidity + '%');
-    newBody.append(date,temp,humid);
-    $(".day1").append(newBody);
-    const newBody2 = $("<div>")
-    const time2 = new Date((response.list[10].dt) * 1000 ).toLocaleDateString("en-US");
-    const date2 = $("<div>").text(time2);
-    const temp2 = $("<div>").text('Temp: ' + response.list[10].main.temp + '°F');
-    const humid2 = $("<div>").text('Humidity: ' + response.list[10].main.humidity + '%');
-    newBody2.append(date2,temp2,humid2);
-    $(".day2").append(newBody2);
-    const newBody3 = $("<div>")
-    const time3 = new Date((response.list[20].dt) * 1000 ).toLocaleDateString("en-US");
-    const date3 = $("<div>").text(time3);
-    const temp3 = $("<div>").text('Temp: ' + response.list[20].main.temp + '°F');
-    const humid3 = $("<div>").text('Humidity: ' + response.list[20].main.humidity + '%');
-    newBody3.append(date3,temp3,humid3);
-    $(".day3").append(newBody3);
-    const newBody4 = $("<div>")
-    const time4 = new Date((response.list[30].dt) * 1000 ).toLocaleDateString("en-US");
-    const date4 = $("<div>").text(time4);
-    const temp4 = $("<div>").text('Temp: ' + response.list[30].main.temp + '°F');
-    const humid4 = $("<div>").text('Humidity: ' + response.list[30].main.humidity + '%');
-    newBody4.append(date4,temp4,humid4);
-    $(".day4").append(newBody4);
-    const newBody5 = $("<div>")
-    const time5 = new Date((response.list[39].dt) * 1000 ).toLocaleDateString("en-US");
-    const date5 = $("<div>").text(time5);
-    const temp5 = $("<div>").text('Temp: ' + response.list[39].main.temp + '°F');
-    const humid5 = $("<div>").text('Humidity: ' + response.list[39].main.humidity + '%');
-    newBody5.append(date5,temp5,humid5);
-    $(".day5").append(newBody5);
-})
-});
+        // $("#historydisplay").append(button);
+        // const historyDisplayBtnEl = document.getElementById("historydisplaybtn");
+        // console.log(historyDisplayBtnEl);
 
-$("#citysearchnow").on("click", function(event){
-    event.preventDefault();
-    const name = $("#inputcity").val();
-    $(".savedcitysearches").empty();
-    const historyArray = [];
-    for (let i = 0; i < historyArray.length; i++) {
-        const newDiv = $("<div>");
-        const newBtn = $("button");
-        newBtn.addClass("historybtns");
-        newBtn.text(historyArray[i]);
-        newBtn.attr("#inputcity", historyArray[i]);
-        newDiv.append(newBtn);
-        $(".savedcitysearches").append(newDiv);
-    }
-    historyArray.push(name);
-    console.log(historyArray);
-});
-
-
+        // now calling the uv index api via "UVindex"
+        const UVindex = 'http://api.openweathermap.org/data/2.5/uvi?appid=f4c95ff5da6ac45aff62770ee6dbda0c&lat=' + response.coord.lat + '&lon=' + response.coord.lon + '';
+        $.ajax({
+            url: UVindex,
+            method: "GET"
+        }).then(function (response) {
+            const uv = $("<h5 style='color: white;'>" + "UV Index: " + "<p id='uvcolor'></p>" + "</h5>");
+            cardbody1.append(uv);
+            card1.append(cardbody1);
+            if (response.value <= 5) {
+                const uvcolorEl = document.getElementById("uvcolor");
+                uvcolorEl.setAttribute("style", "color: green;");
+                const favorable = response.value + " (Favorable)";
+                $("#uvcolor").append(favorable);
+            } else if (response.value <= 10 && response.value > 5) {
+                const uvcolorEl = document.getElementById("uvcolor");
+                uvcolorEl.setAttribute("style", "color: yellow;");
+                const moderate = response.value + " (Moderate)";
+                $("#uvcolor").append(moderate);
+            } else if (response.value > 10) {
+                const uvcolorEl = document.getElementById("uvcolor");
+                uvcolorEl.setAttribute("style", "color: red;");
+                const severe = response.value + " (Severe)";
+                $("#uvcolor").append(severe);
+            }
+        })
+        // now calling the 5 day forecast api
+        const cityName = $("#searchinput").val();
+        const queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&&APPID=f4c95ff5da6ac45aff62770ee6dbda0c'
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            const futuredisplay = $("<div class='futuredisplay my-2'></div>");
+            // card number 1
+            const card1 = $("<div class='card my-3 mx-2' id='futurecard'></div>");
+            const cardbody1 = $("<div class='card-body'></div>");
+            const date1 = $("<p style='color: white;'>" + new Date((response.list[8].dt) * 1000).toLocaleDateString("en-US") + "</p>");
+            const icon1 = $("<img class=" + "'" + "forecasticon" + "'" + " src=" + "'" + "http://openweathermap.org/img/wn/" + response.list[8].weather[0].icon + ".png" + "'" + ">");
+            const temp1 = $("<p style='color: white;'>" + "Temperature: " + response.list[8].main.temp + "°F" + "</p>");
+            const humidity1 = $("<p style='color: white;'>" + "Humidity: " + response.list[8].main.humidity + "%" + "</p>");
+            // card number 2
+            const card2 = $("<div class='card my-3 mx-2' id='futurecard'></div>");
+            const cardbody2 = $("<div class='card-body'></div>");
+            const date2 = $("<p style='color: white;'>" + new Date((response.list[16].dt) * 1000).toLocaleDateString("en-US") + "</p>");
+            const icon2 = $("<img class=" + "'" + "forecasticon" + "'" + " src=" + "'" + "http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + ".png" + "'" + ">");
+            const temp2 = $("<p style='color: white;'>" + "Temperature: " + response.list[16].main.temp + "°F" + "</p>");
+            const humidity2 = $("<p style='color: white;'>" + "Humidity: " + response.list[16].main.humidity + "%" + "</p>");
+            // card number 3
+            const card3 = $("<div class='card my-3 mx-2' id='futurecard'></div>");
+            const cardbody3 = $("<div class='card-body'></div>");
+            const date3 = $("<p style='color: white;'>" + new Date((response.list[24].dt) * 1000).toLocaleDateString("en-US") + "</p>");
+            const icon3 = $("<img class=" + "'" + "forecasticon" + "'" + " src=" + "'" + "http://openweathermap.org/img/wn/" + response.list[24].weather[0].icon + ".png" + "'" + ">");
+            const temp3 = $("<p style='color: white;'>" + "Temperature: " + response.list[24].main.temp + "°F" + "</p>");
+            const humidity3 = $("<p style='color: white;'>" + "Humidity: " + response.list[24].main.humidity + "%" + "</p>");
+            // card number 4
+            const card4 = $("<div class='card my-3 mx-2' id='futurecard'></div>");
+            const cardbody4 = $("<div class='card-body'></div>");
+            const date4 = $("<p style='color: white;'>" + new Date((response.list[32].dt) * 1000).toLocaleDateString("en-US") + "</p>");
+            const icon4 = $("<img class=" + "'" + "forecasticon" + "'" + " src=" + "'" + "http://openweathermap.org/img/wn/" + response.list[32].weather[0].icon + ".png" + "'" + ">");
+            const temp4 = $("<p style='color: white;'>" + "Temperature: " + response.list[32].main.temp + "°F" + "</p>");
+            const humidity4 = $("<p style='color: white;'>" + "Humidity: " + response.list[32].main.humidity + "%" + "</p>");
+            // card number 5
+            const card5 = $("<div class='card my-3 mx-2' id='futurecard'></div>");
+            const cardbody5 = $("<div class='card-body'></div>");
+            const date5 = $("<p style='color: white;'>" + new Date((response.list[39].dt) * 1000).toLocaleDateString("en-US") + "</p>");
+            const icon5 = $("<img class=" + "'" + "forecasticon" + "'" + " src=" + "'" + "http://openweathermap.org/img/wn/" + response.list[39].weather[0].icon + ".png" + "'" + ">");
+            const temp5 = $("<p style='color: white;'>" + "Temperature: " + response.list[39].main.temp + "°F" + "</p>");
+            const humidity5 = $("<p style='color: white;'>" + "Humidity: " + response.list[39].main.humidity + "%" + "</p>");
+            // appending all the cards to the futuredisplay. Theres alot bc a for loop wouldnt be practical.
+            const fiveday = $("<h2 style='color: white;'>5 Day Forecast:</h2>");
+            cardbody1.append(date1, icon1, temp1, humidity1);
+            cardbody2.append(date2, icon2, temp2, humidity2);
+            cardbody3.append(date3, icon3, temp3, humidity3);
+            cardbody4.append(date4, icon4, temp4, humidity4);
+            cardbody5.append(date5, icon5, temp5, humidity5);
+            card1.append(cardbody1);
+            card2.append(cardbody2);
+            card3.append(cardbody3);
+            card4.append(cardbody4);
+            card5.append(cardbody5);
+            futuredisplay.append(fiveday, card1, card2, card3, card4, card5);
+            jumbotron.append(futuredisplay);
+        })
+    })
+}
